69e2fb357f0d12c4275a7387bb41207be7b599a2
Zacoriot <isaacjbrooks@gmail.com>
2024-11-15 13:50:47 +0000

    Created Input Manager

---
+++ Source/Game/Managers/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// InputManager Script.
+/// </summary>
+public class InputManager : Script
+{
+    // INPUT AXIS
+    static InputAxis _VerticalInputAxis;
+    static InputAxis _HorizontalInputAxis;
+
+    // INPUT ACTIONS
+
+    public override void OnAwake()
+    {
+        _VerticalInputAxis = new InputAxis("Vertical");
+        _HorizontalInputAxis = new InputAxis("Horizontal");
+    }
+
+
+    public override void OnDestroy()
+    {
+        _VerticalInputAxis.Dispose();
+        _HorizontalInputAxis.Dispose();
+    }
+
+    public static Vector2 GetMovementAxis()
+    {
+        return new Vector2(_HorizontalInputAxis.ValueRaw, _VerticalInputAxis.ValueRaw);
+    }
+}
---
+++ Source/Game/Player/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// PlayerMovement Script.
+/// </summary>
+public class PlayerMovement : Script
+{
+    public override void OnUpdate()
+    {
+        Debug.Log(InputManager.GetMovementAxis());
+    }
+}
