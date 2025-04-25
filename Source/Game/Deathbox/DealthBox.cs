8353f612f5256f2ca8f95e3e86596948941688b6
Isaac Brooks <isaacjbrooks@gmail.com>
2025-04-25 12:07:08 +0100

    a

---
+++ Source/Game/Deathbox/DealthBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// DealthBox Script.
+/// </summary>
+public class DealthBox : Script
+{
+    [Serialize, ShowInEditor] SceneReference _Scene;
+
+    public override void OnEnable()
+    {
+        Actor.As<Collider>().TriggerEnter += OnTriggerEnter;
+    }
+
+    public override void OnDisable()
+    {
+        Actor.As<Collider>().TriggerEnter -= OnTriggerEnter;
+    }
+
+    void OnTriggerEnter(PhysicsColliderActor collider)
+    {
+        if (collider.Parent.HasTag("Player"))
+        {
+            Level.ChangeSceneAsync(_Scene);
+        }
+    }
+
+}
